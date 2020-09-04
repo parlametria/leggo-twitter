8961f1a250d363e31d24b42dbad02682e78e8e8a
Jefferson Neves <jefferson.rpn@gmail.com>
2020-09-04 15:47:39 -0300

    Prepara para Heroku

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("heroku-logger");
+const forceSsl = require('force-ssl-heroku');
 
 const status = require("./server/config/status");
 const tweets = require("./server/routes/tweets");
 
 const app = express();
+app.use(forceSsl);
 
 const corsOptions = {
   origin: ['http://localhost:4200', 'http://localhost:3000'],
@@ -25,4 +28,4 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+app.listen(port, () => logger.info(`Servidor rodando na porta ${port}`));
