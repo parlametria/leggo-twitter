c60bb2342d73beb3f5c009dbcc933cb379460453
Jefferson Neves <jefferson.rpn@gmail.com>
2020-09-17 10:04:15 -0300

    Renomeia endpointde tweets de parlamentares

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -7,26 +7,26 @@ const models = require("../models/index");
 
 const Tweet = models.tweet;
 
-router.get("/", (req, res) => {
+router.get("/parlamentares", (req, res) => {
   Tweet.findAll({
     group: ["id_parlamentar_parlametria"],
     attributes: [
       "id_parlamentar_parlametria",
-      [Sequelize.fn("COUNT", Sequelize.col("id_parlamentar_parlametria")), "atividade"]
+      [Sequelize.fn("COUNT", Sequelize.col("id_parlamentar_parlametria")), "atividade_twitter"]
     ]
   })
   .then(tweets => res.status(status.SUCCESS).json(tweets))
   .catch(err => res.status(status.BAD_REQUEST).json({ err }));
 });
 
-router.get("/parlamentar/:id_parlamentar", (req, res) => {
+router.get("/parlamentares/:id_parlamentar", (req, res) => {
   const id_parlamentar = req.params.id_parlamentar;
 
   Tweet.findAll({
     group: ["id_parlamentar_parlametria"],
     attributes: [
       "id_parlamentar_parlametria",
-      [Sequelize.fn("COUNT", Sequelize.col("id_parlamentar_parlametria")), "atividade"]
+      [Sequelize.fn("COUNT", Sequelize.col("id_parlamentar_parlametria")), "atividade_twitter"]
     ],
     where: {
       id_parlamentar_parlametria: id_parlamentar
