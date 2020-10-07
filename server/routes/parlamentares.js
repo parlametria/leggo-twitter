9ddfff16de427d5cf2e6702f7a689202199d44b5
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2020-10-07 11:25:08 -0300

    media por dia

--- server/routes/parlamentares.js
+++ server/routes/parlamentares.js
@@ -47,4 +47,57 @@ router.get("/:id_parlamentar", (req, res) => {
     .catch((err) => res.status(status.BAD_REQUEST).json({ err }));
 });
 
+
+// média por dia de tweets
+// datas teste: 06-30-2019 - 12-30-2019
+// mes - dia - ano 
+router.get("/:id_parlamentar/media", (req, res) => {
+
+  const id_parlamentar = req.params.id_parlamentar;
+
+  let dataInicial = req.query.data_inicial;
+
+  let dataFinal = req.query.data_final;
+
+  var dataInicialSplit = new Date(dataInicial); 
+
+  var dataFinalSplit = new Date(dataFinal); 
+
+  var diferenca = dataFinalSplit.getTime() - dataInicialSplit.getTime();
+  var diferenca_dias = diferenca / (1000 * 3600 * 24);
+
+  console.log("datas");
+  console.log(diferenca_dias);
+  
+  let whereClause = {
+    created_at: {
+      [Sequelize.Op.between]: [dataInicial, dataFinal]
+    }
+  }
+  Tweet.findAll({
+    group: ["id_parlamentar_parlametria"],
+    attributes: [
+      "id_parlamentar_parlametria",
+      [
+        Sequelize.fn('COUNT', Sequelize.col("id_parlamentar_parlametria")), 
+        'atividade_twitter'
+      ],
+    ],
+    where: whereClause
+  })
+    .then((tweets) => {
+      const result = tweets.map(tweets => {
+        let data = tweets.toJSON();
+        console.log(data);
+        data['media_tweets'] = data['atividade_twitter']/diferenca_dias;
+        return data;
+      });
+      res.status(status.SUCCESS).json(result);
+    })
+    .catch((err) => res.status(status.BAD_REQUEST).json({ err }));
+  });
+  
+  module.exports = router;
+
+
 module.exports = router;
