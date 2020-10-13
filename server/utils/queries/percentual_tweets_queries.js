0413f1f076091e407c6ef5a6eee248f3dd88701b
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-10-13 16:40:10 -0300

    Desconsidera semana e ano nos dados por tema

--- server/utils/queries/percentual_tweets_queries.js
+++ server/utils/queries/percentual_tweets_queries.js
@@ -28,25 +28,19 @@ function QueryPercentualAtividadeAgregadaPorAgendaETema(interesse, tema, dataIni
   "tt.tema_slug, " +
   "tt.atividade_twitter, " +
   "tt.total, " +
-  "tt.id_parlamentar_parlametria, " +
-  "tt.week, " +
-  "tt.year " +
+  "tt.id_parlamentar_parlametria " +
   "FROM (" +
   "SELECT " +
   "res.tema_slug AS tema_slug, " +
   "res.atividade_twitter AS atividade_twitter, " +
   "res.id_parlamentar_parlametria AS id_parlamentar_parlametria, " +
-  "res.week AS week, " +
-  "res.year AS year, " +
   "SUM(res.atividade_twitter) OVER (" +
-  "PARTITION BY res.slug, res.week, res.year, res.id_parlamentar_parlametria) AS total " +
+  "PARTITION BY res.slug, res.id_parlamentar_parlametria) AS total " +
   "FROM (" +
   "SELECT " +
   "agenda.slug, tema.slug AS tema_slug, " +
   "tweet.id_parlamentar_parlametria AS id_parlamentar_parlametria, " +
-  "COUNT(tweet.id_tweet) AS atividade_twitter, " +
-  "date_part('week', created_at) as week, " +
-  "date_part('year', created_at) as year " +
+  "COUNT(tweet.id_tweet) AS atividade_twitter " +
   "FROM tema_proposicao "+
   "INNER JOIN proposicao ON tema_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
   "INNER JOIN agenda_proposicao ON agenda_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
@@ -55,7 +49,7 @@ function QueryPercentualAtividadeAgregadaPorAgendaETema(interesse, tema, dataIni
   "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet AND tweet.created_at BETWEEN '"+
   dataInicial +"' AND '"+ dataFinal + "' " +
   "INNER JOIN tema ON tema_proposicao.id_tema = tema.id " +
-  "GROUP BY tema.slug, agenda.slug, tweet.id_parlamentar_parlametria, week, year) AS res) AS tt " +
+  "GROUP BY tema.slug, agenda.slug, tweet.id_parlamentar_parlametria) AS res) AS tt " +
   "where tt.tema_slug = '" + tema + "';";
 
   return q;
