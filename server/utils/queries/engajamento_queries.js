024c4d5ef7ef9afb144a2c90f566e50101c02cc1
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-10-14 13:25:58 -0300

    Finaliza consulta que retorna engajamento geral por agenda

--- server/utils/queries/engajamento_queries.js
+++ server/utils/queries/engajamento_queries.js
@@ -1,12 +1,8 @@
 
 function QueryEngajamentoAgregadoPorAgenda(interesse, dataInicial, dataFinal, idParlamentar) {
   const q = "SELECT " +
-  "res.id_parlamentar_parlametria, " +
-  "SUM(res.interactions) OVER (PARTITION BY res.slug) AS engajamento FROM (" +
-  "SELECT " +
-  "agenda.slug, " +
   "tweet.id_parlamentar_parlametria AS id_parlamentar_parlametria, " +
-  "SUM(tweet.interactions) AS interactions " +
+  "SUM(tweet.interactions) AS engajamento " +
   "FROM tema_proposicao "+
   "INNER JOIN proposicao ON tema_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
   "INNER JOIN agenda_proposicao ON agenda_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
@@ -14,7 +10,7 @@ function QueryEngajamentoAgregadoPorAgenda(interesse, dataInicial, dataFinal, id
   "INNER JOIN tweet_proposicao ON proposicao.id_proposicao_leggo = tweet_proposicao.id_proposicao_leggo " +
   "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet AND tweet.created_at BETWEEN '"+
   dataInicial +"' AND '"+ dataFinal + "' AND tweet.id_parlamentar_parlametria = '" + idParlamentar + "' " +
-  "GROUP BY agenda.slug, tweet.id_parlamentar_parlametria) AS res;";
+  "GROUP BY agenda.slug, tweet.id_parlamentar_parlametria;";
 
   return q;
 }
