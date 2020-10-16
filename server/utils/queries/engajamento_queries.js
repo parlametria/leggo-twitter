5ecce6f5e77a250d8aeb27bbc31df9bbb78e0554
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-10-16 08:38:21 -0300

    Modifica endpoint de engajamento para todos os parlamentares

--- server/utils/queries/engajamento_queries.js
+++ server/utils/queries/engajamento_queries.js
@@ -1,5 +1,5 @@
 
-function QueryEngajamentoAgregadoPorAgenda(interesse, dataInicial, dataFinal, idParlamentar) {
+function QueryEngajamentoAgregadoPorAgenda(interesse, dataInicial, dataFinal) {
   const q = "SELECT " +
   "tweet.id_parlamentar_parlametria AS id_parlamentar_parlametria, " +
   "SUM(tweet.interactions) AS engajamento " +
@@ -9,15 +9,16 @@ function QueryEngajamentoAgregadoPorAgenda(interesse, dataInicial, dataFinal, id
   "INNER JOIN agenda ON agenda_proposicao.id_agenda = agenda.id AND agenda.slug = '" + interesse + "' " +
   "INNER JOIN tweet_proposicao ON proposicao.id_proposicao_leggo = tweet_proposicao.id_proposicao_leggo " +
   "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet AND tweet.created_at BETWEEN '"+
-  dataInicial +"' AND '"+ dataFinal + "' AND tweet.id_parlamentar_parlametria = '" + idParlamentar + "' " +
+  dataInicial +"' AND '"+ dataFinal + "' " +
   "GROUP BY agenda.slug, tweet.id_parlamentar_parlametria;";
 
   return q;
 }
 
-function QueryEngajamentoAgregadoPorAgendaETema(interesse, tema, dataInicial, dataFinal, idParlamentar) {
+function QueryEngajamentoAgregadoPorAgendaETema(interesse, tema, dataInicial, dataFinal) {
   const q = "SELECT " +
   "tema.slug AS tema_slug, " +
+  "tweet.id_parlamentar_parlametria AS id_parlamentar_parlametria, " +
   "SUM(tweet.interactions) AS engajamento " +
   "FROM tema_proposicao "+
   "INNER JOIN proposicao ON tema_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
@@ -25,9 +26,9 @@ function QueryEngajamentoAgregadoPorAgendaETema(interesse, tema, dataInicial, da
   "INNER JOIN agenda ON agenda_proposicao.id_agenda = agenda.id AND agenda.slug = '" + interesse + "' " +
   "INNER JOIN tweet_proposicao ON proposicao.id_proposicao_leggo = tweet_proposicao.id_proposicao_leggo " +
   "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet AND tweet.created_at BETWEEN '"+
-  dataInicial +"' AND '"+ dataFinal + "' AND tweet.id_parlamentar_parlametria = '" + idParlamentar + "' " +
+  dataInicial +"' AND '"+ dataFinal + "' " +
   "INNER JOIN tema ON tema_proposicao.id_tema = tema.id AND tema.slug = '" + tema + "' " +
-  "GROUP BY tema.slug;";
+  "GROUP BY tema.slug, tweet.id_parlamentar_parlametria;";
 
   return q;
 }
