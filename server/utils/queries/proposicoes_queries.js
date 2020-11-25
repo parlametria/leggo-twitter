f44bfb309b08bb1576ee23578de5aad7b07c931a
Andrezj <andre.oliveira@ccc.ufcg.edu.br>
2020-11-25 15:03:15 -0300

    considera sigla mais completa da tabela de proposicoes

--- server/utils/queries/proposicoes_queries.js
+++ server/utils/queries/proposicoes_queries.js
@@ -1,14 +1,15 @@
 function QueryProposicoesComMaisTweetsPorParlamentar(interesse, dataInicial, dataFinal, id_parlamentar, qtd) {
   const q ="SELECT " +
-    "tweet_proposicao.id_proposicao_leggo, tweet_proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " + 
+    "tweet_proposicao.id_proposicao_leggo, proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " + 
     "FROM " + 
     "tweet_proposicao " +
     "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet " +
     "AND tweet.id_parlamentar_parlametria = '" + id_parlamentar + "' " +
     "AND tweet.created_at BETWEEN '" + dataInicial + "' AND '" + dataFinal + "' " +
+    "INNER JOIN proposicao ON tweet_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
     "INNER JOIN agenda_proposicao ON agenda_proposicao.id_proposicao_leggo = tweet_proposicao.id_proposicao_leggo " +
     "INNER JOIN agenda ON agenda_proposicao.id_agenda = agenda.id AND agenda.slug = '" + interesse + "' " +
-    "GROUP BY tweet_proposicao.id_proposicao_leggo,  tweet_proposicao.sigla " +
+    "GROUP BY tweet_proposicao.id_proposicao_leggo,  proposicao.sigla " +
     "HAVING COUNT(tweet_proposicao.id_tweet) > 0 " +
     "ORDER BY num_tweets DESC " +
     "LIMIT " + qtd + " ";
@@ -18,17 +19,18 @@ function QueryProposicoesComMaisTweetsPorParlamentar(interesse, dataInicial, dat
 
 function QueryProposicoesComMaisTweetsPorTemaEParlamentar(tema, interesse, dataInicial, dataFinal, id_parlamentar, qtd) {
   const q ="SELECT " +
-    "tweet_proposicao.id_proposicao_leggo, tweet_proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " + 
+    "tweet_proposicao.id_proposicao_leggo, proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " + 
     "FROM " + 
     "tweet_proposicao " +
     "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet " +
     "AND tweet.id_parlamentar_parlametria = '" + id_parlamentar + "' " +
     "AND tweet.created_at BETWEEN '" + dataInicial + "' AND '" + dataFinal + "' " +
+    "INNER JOIN proposicao ON tweet_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo " +
     "INNER JOIN agenda_proposicao ON agenda_proposicao.id_proposicao_leggo = tweet_proposicao.id_proposicao_leggo " +
     "INNER JOIN agenda ON agenda_proposicao.id_agenda = agenda.id AND agenda.slug = '" + interesse + "' " +
     "INNER JOIN tema_proposicao on tweet_proposicao.id_proposicao_leggo = tema_proposicao.id_proposicao_leggo " +
     "INNER JOIN tema ON tema_proposicao.id_tema = tema.id AND tema.slug = '" + tema + "' " +
-    "GROUP BY tweet_proposicao.id_proposicao_leggo,  tweet_proposicao.sigla " +
+    "GROUP BY tweet_proposicao.id_proposicao_leggo,  proposicao.sigla " +
     "HAVING COUNT(tweet_proposicao.id_tweet) > 0 " +
     "ORDER BY num_tweets DESC " +
     "LIMIT " + qtd + " ";
