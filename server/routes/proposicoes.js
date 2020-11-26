2bcc4ee7b23708d455fb40661ec7b86338e7ec9e
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-11-26 16:31:46 -0300

    Adiciona query que retorna as proposições mais comentadas por agenda

--- server/routes/proposicoes.js
+++ server/routes/proposicoes.js
@@ -9,7 +9,8 @@ const models = require("../models/index");
 
 const {
   QueryProposicoesComMaisTweetsPorParlamentar,
-  QueryProposicoesComMaisTweetsPorTemaEParlamentar
+  QueryProposicoesComMaisTweetsPorTemaEParlamentar,
+  QueryProposicoesComMaisTweetsPorAgenda,
 } = require("../utils/queries/proposicoes_queries");
 
 router.get("/parlamentar/:id", (req, res) => {
@@ -49,7 +50,28 @@ router.get("/parlamentar/:id", (req, res) => {
       qtd
     );
   }
+});
 
+router.get("/mais-comentadas", (req, res) => {
+  let dataInicial = req.query.data_inicial;
+  let dataFinal = req.query.data_final;
+  let qtd = req.query.qtd;
+
+  dataInicial = moment(dataInicial).format("YYYY-MM-DD");
+  dataFinal = moment(dataFinal).format("YYYY-MM-DD");
+
+  let interesse = req.query.interesse;
+
+  if (typeof qtd === "undefined" || qtd === "") {
+    qtd = "ALL";
+  }
+
+  const query = QueryProposicoesComMaisTweetsPorAgenda(
+    interesse,
+    dataInicial,
+    dataFinal,
+    qtd
+  );
   models.sequelize
     .query(query, {
       type: Sequelize.QueryTypes.SELECT,
--- server/utils/queries/proposicoes_queries.js
+++ server/utils/queries/proposicoes_queries.js
@@ -1,7 +1,7 @@
 function QueryProposicoesComMaisTweetsPorParlamentar(interesse, dataInicial, dataFinal, id_parlamentar, qtd) {
   const q ="SELECT " +
-    "tweet_proposicao.id_proposicao_leggo, proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " + 
-    "FROM " + 
+    "tweet_proposicao.id_proposicao_leggo, proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " +
+    "FROM " +
     "tweet_proposicao " +
     "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet " +
     "AND tweet.id_parlamentar_parlametria = '" + id_parlamentar + "' " +
@@ -19,8 +19,8 @@ function QueryProposicoesComMaisTweetsPorParlamentar(interesse, dataInicial, dat
 
 function QueryProposicoesComMaisTweetsPorTemaEParlamentar(tema, interesse, dataInicial, dataFinal, id_parlamentar, qtd) {
   const q ="SELECT " +
-    "tweet_proposicao.id_proposicao_leggo, proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " + 
-    "FROM " + 
+    "tweet_proposicao.id_proposicao_leggo, proposicao.sigla, COUNT(tweet_proposicao.id_tweet) AS num_tweets " +
+    "FROM " +
     "tweet_proposicao " +
     "INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet " +
     "AND tweet.id_parlamentar_parlametria = '" + id_parlamentar + "' " +
@@ -38,7 +38,28 @@ function QueryProposicoesComMaisTweetsPorTemaEParlamentar(tema, interesse, dataI
   return q;
 }
 
+function QueryProposicoesComMaisTweetsPorAgenda(interesse, dataInicial, dataFinal, qtd) {
+  const q =`SELECT
+      tweet_proposicao.id_proposicao_leggo,
+      proposicao.sigla,
+      COUNT(DISTINCT(tweet_proposicao.id_tweet)) AS num_tweets
+    FROM
+      tweet_proposicao
+      INNER JOIN tweet ON tweet_proposicao.id_tweet = tweet.id_tweet
+      AND tweet.created_at BETWEEN '${dataInicial}' AND '${dataFinal}'
+      INNER JOIN proposicao ON tweet_proposicao.id_proposicao_leggo = proposicao.id_proposicao_leggo
+      INNER JOIN agenda_proposicao ON agenda_proposicao.id_proposicao_leggo = tweet_proposicao.id_proposicao_leggo
+      INNER JOIN agenda ON agenda_proposicao.id_agenda = agenda.id AND agenda.slug = '${interesse}'
+    GROUP BY tweet_proposicao.id_proposicao_leggo,  proposicao.sigla
+    HAVING COUNT(tweet_proposicao.id_tweet) > 0
+    ORDER BY num_tweets DESC
+    LIMIT ${qtd};`
+    ;
+  return q;
+}
+
 module.exports = {
   QueryProposicoesComMaisTweetsPorParlamentar,
-  QueryProposicoesComMaisTweetsPorTemaEParlamentar
+  QueryProposicoesComMaisTweetsPorTemaEParlamentar,
+  QueryProposicoesComMaisTweetsPorAgenda
 }
