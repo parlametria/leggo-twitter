48cf9ce9b7d67a44cd318bb2c9948b62bec83dba
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-05-07 12:56:01 -0300

    Adiciona endpoint que retorna o número de tweets e interações por proposição

--- server/routes/proposicoes.js
+++ server/routes/proposicoes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const Sequelize = require("sequelize");
+const { Sequelize, Op } = require("sequelize");
 const router = express.Router();
 
 const moment = require("moment");
 
 const status = require("../config/status");
 const models = require("../models/index");
+
+const Tweet = models.tweet;
+const TweetProposicao = models.tweet_proposicao;
 
 const {
   QueryProposicoesComMaisTweetsPorParlamentar,
@@ -92,6 +95,42 @@ router.get("/mais-comentadas", (req, res) => {
       res.status(status.SUCCESS).json(tweets);
     })
     .catch((err) => res.status(status.BAD_REQUEST).json({ err }));
+});
+
+
+router.get("/", (req, res) => {
+  let dataInicial = req.query.data_inicial;
+  let dataFinal = req.query.data_final;
+
+  dataInicial = moment(dataInicial).format("YYYY-MM-DD");
+  dataFinal = moment(dataFinal).format("YYYY-MM-DD");
+
+  TweetProposicao.findAll({
+    attributes: [
+      'id_proposicao_leggo',
+      [Sequelize.fn('COUNT', Sequelize.col('tweet_proposicao.id_tweet')) ,'num_tweets'],
+      [Sequelize.fn('SUM', Sequelize.col('tweet.interactions')) ,'interactions']
+    ],
+    group: ['tweet_proposicao.id_proposicao_leggo'],
+    includeIgnoreAttributes: false,
+    include: [
+      {
+        model: Tweet,
+        attributes: ['interactions'],
+        where: {
+          created_at: {
+            [Op.between]: [dataInicial, dataFinal]
+          }
+        },
+        require: true
+      }
+    ]
+   }
+  )
+  .then((tweets) => {
+    res.status(status.SUCCESS).json(tweets);
+  })
+  .catch((err) => res.status(status.BAD_REQUEST).json({ err }));
 });
 
 module.exports = router;
